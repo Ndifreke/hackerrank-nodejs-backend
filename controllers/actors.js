6a6a0ff7e46f36cf0ba23c5de712ac4440e6ecb8
Ndifreke Ekim <ndifreke.ekim@andela.com>
2019-09-22 00:43:13 +0100

    chore(database-normalize): normalize event model
    - enforce referential integrity between events actors model
    - enforce referential integrity between events and repositories model

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,6 +1,6 @@
 
 var getAllActors = () => {
-	
+
 };
 
 var updateActor = () => {
@@ -11,8 +11,18 @@ var getStreak = () => {
 
 };
 
+const addActor = async (actors, data) => {
+	try {
+		await actors.create(data)
+		return { created: true, message: "succesfull" };
+	} catch (e) {
+		console.log(__filename, e)
+		return { created: false, message: e }
+	}
+}
 
 module.exports = {
+	addActor,
 	updateActor: updateActor,
 	getAllActors: getAllActors,
 	getStreak: getStreak
--- controllers/events.js
+++ controllers/events.js
@@ -1,19 +1,47 @@
 
-const { events } = require("../models/index");
+const { events, actors, repositories } = require("../models/index");
+const { addRepository } = require("./repo")
+const { addActor } = require("./actors")
 
 var getAllEvents = () => {
 
 };
 
-var addEvent = async (req, resp, next) => {
-	const { id, type, actor_id } = req.body;
-	//console.log(req.body)
+const createEvent = async (eventsModel, data) => {
 	try {
-		await events.create({ id, type, actor_id })
-		resp.statusCode = 201
+		await eventsModel.create(data)
+		return { created: true, message: "succesfull" };
 	} catch (e) {
-		console.log(e.message)
-		resp.statusCode = 400
+		console.log(__filename, e)
+		return { created: false, message: e }
+	}
+}
+
+var addEvent = async (req, resp, next) => {
+	const { id, type, actor: actorData, repo: repoData } = req.body;
+	try {
+		console.log(req.body)
+		await addActor(actors, actorData)
+		await addRepository(repositories, { ...repoData, actor_id: actorData.id })
+
+		//to create an event, you need a repo and actor id
+		const result = await createEvent(events, {
+			id,
+			type,
+			actor_id: actorData.id,
+			repository_id: repoData.id
+		})
+
+		if (result.created) {
+			resp.statusCode = 201
+		} else {
+			resp.statusCode = 400
+			//	console.log(result.message)
+		}
+	} catch (e) {
+		//internal server Error
+		resp.statusCode = 500
+		next(req, resp)
 	}
 	resp.end()
 };
@@ -34,7 +62,9 @@ var eraseEvents = async (req, resp, next) => {
 		resp.statusCode = 200
 		resp.end()
 	} catch (e) {
-		next(e)
+		//internal server Error
+		resp.statusCode = 500
+		next(req, resp)
 	}
 };
 
