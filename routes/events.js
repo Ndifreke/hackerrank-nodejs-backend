ed9f9892bad3b1e528963ef7d77b48d9b2eccd5e
Ndifreke Ekim <ndifreke.ekim@andela.com>
2019-09-21 23:09:27 +0100

    kdkek

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const { event } = require("../controllers/index")
+
 
 // Routes related to event
 
+router.post("/", event.addEvent)
 
 module.exports = router;
